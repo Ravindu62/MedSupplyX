35addc9fcd60927f2647a025227c57e3ace1f6a7
K.N.W.PEIRIS <wishwajithnisal@gmail.com>
2024-02-07 02:38:46 +0530

    Feature - Supplier UI Feature : User Registration UI

---
+++ app/views/supplier/advertistments.php
@@ -0,0 +1,79 @@
+
+
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Advetistment </title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style.css">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7063445217024139"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<?php require APPROOT . '/views/inc/supplier_sidebar.php'; ?>
+
+
+<!-- content -->
+  <div class="content">
+
+ <div class="anim"> <h2> Advertistments </h2> </div>
+
+
+  <div class="anim"> 
+  <p class="text_centered"> Add nice description about the medicine that you need to advertise </p>
+  </div>
+
+<div class="anim">  
+<section class="contact-form">
+        <div class="form-container2">
+            <h2>Your Details</h2>
+            <form action="#" method="POST">
+ 
+                <label for="name"> Heading : </label>
+                <input type="text" id="name" name="name" required>
+ 
+                <label for="message"> Description: </label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+ 
+                <button type="submit" class="submit-button"> Post </button>
+            </form>
+        </div>
+    </section>
+    </div>
+
+    <div class="anim">  <h2> Previous Posts </h2> </div>
+
+
+
+    
+<!-- google adds-->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7063445217024139"
+     crossorigin="anonymous"></script>
+<!-- mesupplyX -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-7063445217024139"
+     data-adtest="on"
+     data-ad-slot="4877917383"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+  
+
+</div>
+</div>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
+
+</body>
+</html>
+
