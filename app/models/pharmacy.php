3ba51ce1a0c0d6cfb674edc451a71ff7cb5a46a9
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-02-27 02:25:42 +0530

    Count accept orders funtion added

--- app/models/pharmacy.php
+++ app/models/pharmacy.php
@@ -80,4 +80,16 @@ class pharmacy
             return 0;
                 }
     }
+
+    public function countAcceptedOrders() {
+        $this->query = $this->db->query2("SELECT COUNT(*) as count FROM approvedorders");
+    
+        if ($this->query) {
+            return $this->query[0]->count;
+           
+        } else {
+            // Handle the error, e.g., log it or return an appropriate value
+            return 0;
+                }
+    }
 }
