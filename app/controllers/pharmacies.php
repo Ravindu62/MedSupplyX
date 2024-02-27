2e1d2c5c3a95902688f9f6727ff098e5c6788eef
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-02-27 16:15:35 +0530

    changes added to index method to funtion with different users

--- app/controllers/pharmacies.php
+++ app/controllers/pharmacies.php
@@ -1,19 +1,27 @@
 <?php
- class Pharmacies extends Controller {
+class Pharmacies extends Controller
+{
 
     public $pharmacyModel;
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->pharmacyModel = $this->model('Pharmacy');
     }
 
-    public function index() {
-        
-$countTotalOrders = $this->pharmacyModel->countTotalOrders();
-        $countAcceptedOrders = $this->pharmacyModel->countAcceptedOrders();
-        $countOutOfStockProducts = $this->pharmacyModel->countOutOfStockProducts();
-        $countExpiredOrders = $this->pharmacyModel->countExpiredOrders();
+    public function index()
+    {
+        //sanitize post inputs
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        $data1 = trim($_SESSION['USER_DATA']['name']);
+        $data2 = trim($_SESSION['USER_DATA']['id']);
+
+        $countTotalOrders = $this->pharmacyModel->countTotalOrders($data1);
+        $countAcceptedOrders = $this->pharmacyModel->countAcceptedOrders($data1);
+        $countOutOfStockProducts = $this->pharmacyModel->countOutOfStockProducts($data2);
+        $countExpiredOrders = $this->pharmacyModel->countExpiredOrders($data2);
 
         $data = [
             'countTotalOrders' => $countTotalOrders,
@@ -27,60 +35,64 @@ $countTotalOrders = $this->pharmacyModel->countTotalOrders();
     }
 
 
-    public function inventory() {
+    public function inventory()
+    {
         $data = [];
-        
+
         $this->view('pharmacy/inventory', $data);
     }
 
-    public function messages() {
+    public function messages()
+    {
         $data = [];
-        
+
         $this->view('pharmacy/messages', $data);
     }
 
-    public function advertistments() {
+    public function advertistments()
+    {
         $data = [];
-        
+
         $this->view('pharmacy/advertistments', $data);
     }
 
-    public function orders() {   
+    public function orders()
+    {
 
-        $data1=[
-        'pharmacyname' => trim($_SESSION['USER_DATA']['name'])
+        $data1 = [
+            'pharmacyname' => trim($_SESSION['USER_DATA']['name'])
         ];
 
         $this->pharmacyModel->getOrder($data1);
         $order = $this->pharmacyModel->getOrder();
 
         $data = [
-            'order' => $order ,
+            'order' => $order,
 
         ];
-        
-        $this->view('pharmacy/orders', $data);
 
-       
+        $this->view('pharmacy/orders', $data);
     }
 
-    public function customerOrders() {
+    public function customerOrders()
+    {
         $data = [];
-        
+
         $this->view('pharmacy/customerOrders', $data);
     }
 
-    public function deleteOrder($id) {
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    public function deleteOrder($id)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Get existing post from model
             $order = $this->pharmacyModel->getOrderById($id);
 
             // Check for owner
-            if($order->pharmacyname != $_SESSION['USER_DATA']['name']) {
+            if ($order->pharmacyname != $_SESSION['USER_DATA']['name']) {
                 redirect('pharmacies/orders');
             }
 
-            if($this->pharmacyModel->deleteOrder($id)) {
+            if ($this->pharmacyModel->deleteOrder($id)) {
                 redirect('pharmacies/orders');
             } else {
                 die('Something went wrong');
@@ -88,16 +100,18 @@ $countTotalOrders = $this->pharmacyModel->countTotalOrders();
         } else {
             redirect('pharmacies/orders');
         }
-}
+    }
 
 
-    public function history() {
+    public function history()
+    {
         $data = [];
-        
+
         $this->view('pharmacy/history', $data);
     }
 
-    public function profile() {
+    public function profile()
+    {
         $pharmacyname = trim($_SESSION['USER_DATA']['name']);
         $this->pharmacyModel->getProfileData($pharmacyname);
         $profile = $this->pharmacyModel->getProfileData();
@@ -105,11 +119,12 @@ $countTotalOrders = $this->pharmacyModel->countTotalOrders();
         $data = [
             'profile' => $profile
         ];
-        
+
         $this->view('pharmacy/profile', $data);
     }
 
-    public function logout(){
+    public function logout()
+    {
         unset($_SESSION['user_id']);
         unset($_SESSION['user_email']);
         unset($_SESSION['user_name']);
@@ -117,11 +132,12 @@ $countTotalOrders = $this->pharmacyModel->countTotalOrders();
         redirect('users/login');
     }
 
-    public function new_order() {
-    
+    public function new_order()
+    {
+
 
         // Check for POST
-        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
 
             // Sanitize POST data
@@ -142,72 +158,66 @@ $countTotalOrders = $this->pharmacyModel->countTotalOrders();
             ];
 
             // Validate data
-            if(empty($data['Mname'])) {
+            if (empty($data['Mname'])) {
                 $data['Mname_err'] = 'Please enter medicine name';
             }
 
-            if(empty($data['Bnum'])) {
+            if (empty($data['Bnum'])) {
                 $data['Bnum_err'] = 'Please enter batch number';
             }
 
-            if(empty($data['quantity'])) {
+            if (empty($data['quantity'])) {
                 $data['quantity_err'] = 'Please enter quantity';
             }
 
-            if(empty($data['ddate'])) {
+            if (empty($data['ddate'])) {
                 $data['ddate_err'] = 'Please enter delivery date';
             }
 
-            if(empty($data['oedate'])) {
+            if (empty($data['oedate'])) {
                 $data['oedent_err'] = 'Please enter order entry date';
             }
 
             // Make sure no errors
-            if(empty($data['Mname_err']) && empty($data['Bnum_err']) && empty($data['quantity_err']) && empty($data['ddate_err']) && empty($data['oedent_err'])) {
+            if (empty($data['Mname_err']) && empty($data['Bnum_err']) && empty($data['quantity_err']) && empty($data['ddate_err']) && empty($data['oedent_err'])) {
                 // Validated
 
                 // Check and set logged in user
-                if(isset($_SESSION['user_id'])) {
+                if (isset($_SESSION['user_id'])) {
                     $data['user_id'] = $_SESSION['user_id'];
                 } else {
                     $data['user_id'] = 0;
                 }
 
                 // Register user from model function
-                if($this->pharmacyModel->addOrder($data)) {
+                if ($this->pharmacyModel->addOrder($data)) {
                     // Redirect to order
                     redirect('pharmacies/orders');
                 } else {
                     die('Something went wrong');
                 }
-
             } else {
                 // Load view with errors
                 $this->view('pharmacy/new_order', $data);
             }
-    } else {
-        // Init data
-        $data = [
-            'Mname' => '',
-            'Bnum' => '',
-            'quantity' => '',
-            'ddate' => '',
-            'oedate' => '',
-            'username' => '',
-            'Mname_err' => '',
-            'Bnum_err' => '',
-            'quantity_err' => '',
-            'ddate_err' => '',
-            'oedate_err' => ''
-        ];
+        } else {
+            // Init data
+            $data = [
+                'Mname' => '',
+                'Bnum' => '',
+                'quantity' => '',
+                'ddate' => '',
+                'oedate' => '',
+                'username' => '',
+                'Mname_err' => '',
+                'Bnum_err' => '',
+                'quantity_err' => '',
+                'ddate_err' => '',
+                'oedate_err' => ''
+            ];
 
-        // Load view
-        $this->view('pharmacy/new_order', $data);
+            // Load view
+            $this->view('pharmacy/new_order', $data);
+        }
     }
 }
-
-
-
-
-}
-?>
