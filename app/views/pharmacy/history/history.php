0308f3a97c23590ded30fefe4dcd70f7611cf63a
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-03-01 03:07:02 +0530

    created new folder and add history.php to it. canceled orders now works

--- app/views/pharmacy/history/history.php
+++ app/views/pharmacy/history/history.php
@@ -1,101 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title> History </title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style.css">
+  <title> History </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style.css">
 </head>
+
 <body>
 
 
-<?php require APPROOT . '/views/inc/header.php'; ?>
+  <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<?php require APPROOT . '/views/inc/pharmacy_sidebar.php'; ?>
+  <?php require APPROOT . '/views/inc/pharmacy_sidebar.php'; ?>
 
-<!-- content -->
+  <!-- content -->
   <div class="content">
 
-  <div class="anim">  <h2> Delivered Orders </h2> </div> 
+    <div class="anim">
+      <h2> Delivered Orders </h2>
+    </div>
 
-<div class="anim">  
-<table class="customers">
-  <tr>
-    <th class="custom1"> Medicine Id </th>
-    <th class="custom1"> Medicine Name </th>
-    <th class="custom1"> Batch No </th>
-    <th class="custom1"> Category </th>
-    <th class="custom1"> Quantity </th>
-    <th class="custom1"> Ordered Date </th>
-    <th class="custom1"> Delivered Date </th>
-    <th class="custom1"> Supplier Name </th>
-    <th class="custom1"> Price (LKR) </th>
-   
-    
-    
-  </tr>
-<?php foreach($data['deliveredHistory'] as $deliveredOrders) : ?>
-<tr> 
-  <td> <?php echo $deliveredOrders->medicine_id; ?></td>
-  <td> <?php echo $deliveredOrders->medicine_name; ?></td>
-  <td> <?php echo $deliveredOrders->batchno; ?></td>
-  <td> <?php echo $deliveredOrders->category; ?></td>
-  <td> <?php echo $deliveredOrders->ordered_date; ?></td>
-  <td> <?php echo $deliveredOrders->deliveryDate; ?></td>
-  <td> <?php echo $deliveredOrders->supplier_name; ?></td>
-  <td> <?php echo $deliveredOrders->price; ?></td>
-</tr>
-<?php endforeach; ?>
-</table>
-</div>
+    <div class="anim">
+      <table class="customers">
+        <tr>
+          <th class="custom1"> Medicine Id </th>
+          <th class="custom1"> Medicine Name </th>
+          <th class="custom1"> Batch No </th>
+          <th class="custom1"> Category </th>
+          <th class="custom1"> Quantity </th>
+          <th class="custom1"> Ordered Date </th>
+          <th class="custom1"> Delivered Date </th>
+          <th class="custom1"> Supplier Name </th>
+          <th class="custom1"> Price (LKR) </th>
 
 
 
-<div class="space"></div>
-<div class="anim">  
-<h2> Cancelled Orders (By You!) </h2>
-</div>
-<div class="anim">   
-<table class="customers">
-  <tr>
-    <th class="custom3"> </th>
-    <th class="custom3"> Medicine Name </th>
-    <th class="custom3"> Pharmacy Name </th>
-    <th class="custom3"> Batch No </th>
-    <th class="custom3"> Quantity </th>
-    <th class="custom3"> Reason for cancelling </th>
-    
-    </tr>
-<tr> 
-  <td> </td>
-  <td> </td>
-  <td> </td>
-  <td> </td>
-  <td> </td>
-  <td> </td>
-  <td> </td>
-</tr>
+        </tr>
+        <?php foreach ($data['deliveredHistory'] as $deliveredOrders) : ?>
+          <tr>
+            <td> <?php echo $deliveredOrders->medicine_id; ?></td>
+            <td> <?php echo $deliveredOrders->medicine_name; ?></td>
+            <td> <?php echo $deliveredOrders->batchno; ?></td>
+            <td> <?php echo $deliveredOrders->category; ?></td>
+            <td> <?php echo $deliveredOrders->ordered_date; ?></td>
+            <td> <?php echo $deliveredOrders->deliveryDate; ?></td>
+            <td> <?php echo $deliveredOrders->supplier_name; ?></td>
+            <td> <?php echo $deliveredOrders->price; ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </table>
+    </div>
 
-<tr> 
-  <td> 01</td>
-  <td> Panadol </td>
-  <td> 1231</td>
-  <td> 1232</td>
-  <td> 200 </td>
-  <td> i received another panadol order.</td>
 
- 
-</tr>
 
-</table>
-</div>
+    <div class="space"></div>
+    <div class="anim">
+      <h2> Cancelled Orders (By You!) </h2>
+    </div>
+    <div class="anim">
+      <table class="customers">
+        <tr>
+          <th class="custom3"> Medicine Id</th>
+          <th class="custom3"> Medicine Name </th>
+          <th class="custom3"> Supplier Name </th>
+          <th class="custom3"> Batch No </th>
+          <th class="custom3"> Category </th>
+          <th class="custom3"> Quantity </th>
+          <th class="custom3"> Reason for cancelling </th>
 
-</div>
-</div>
+        </tr>
+        <?php foreach ($data['canceledHistory'] as $canceledOrders) : ?>
+          <tr>
+            <td> <?php echo $canceledOrders->medicine_id; ?></td>
+            <td> <?php echo $canceledOrders->medicine_name; ?></td>
+            <td> <?php echo $canceledOrders->supplier_name; ?></td>
+            <td> <?php echo $canceledOrders->batchno; ?></td>
+            <td> <?php echo $canceledOrders->category; ?></td>
+            <td> <?php echo $canceledOrders->quantity; ?></td>
+            <td> <?php echo $canceledOrders->reason; ?></td>
+          </tr>
+        <?php endforeach; ?>
 
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+      </table>
+    </div>
+
+  </div>
+  </div>
+
+  <?php require APPROOT . '/views/inc/footer.php'; ?>
 
 
 </body>
-</html>
 
+</html>
