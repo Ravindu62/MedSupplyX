344c0bb72ac9810291fac874fd44f3113155ce3f
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-03-11 10:16:33 +0530

    add php to fetch the record data to the view

--- app/views/pharmacy/inventory/editInventory.php
+++ app/views/pharmacy/inventory/editInventory.php
@@ -25,28 +25,29 @@
         <div class="anim">
             <div class="container-fluid">
                 <div class="d-flex">
-                    <form action="<?php echo URLROOT; ?>/pharmacies/addInventory" method="POST" class="orderform">
+                    <form action="<?php echo URLROOT; ?>/pharmacies/editInventory" method="POST" class="orderform">
 
                         <table>
 
                             <tr>
                                 <td colspan="2">
-                                    <h3> <br> <br> Medicine Details </h3>
+                                    <h3> <br> Medicine Details</h3>
                                 </td>
                             </tr>
+
                             <tr>
                                 <td class="verticleCentered">
                                     Medicine ID
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="text" name="medicineId" class="orderdetails" required> </td>
+                                <td class="verticleCentered"> <input type="text" name="medicineId" class="orderdetails" value="<?php echo $data->medicineId; ?>" required>  </td>
 
 
                                 <td class="verticleCentered">
                                     Medicine Name
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"><input type="text" name="medicineName" class="smallForm" required> </td>
+                                <td class="verticleCentered"><input type="text" name="medicineName" class="smallForm" value="<?php echo $data->medicineName; ?>" required> </td>
 
                             </tr>
 
@@ -55,13 +56,13 @@
                                     Batch No
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="text" name="batchNo" class="orderdetails" required> </td>
+                                <td class="verticleCentered"> <input type="text" name="batchNo" class="orderdetails" value="<?php echo $data->batchNo; ?>" required> </td>
 
                                 <td class="verticleCentered">
                                     Category
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="text" name="category" class="smallForm" required> </td>
+                                <td class="verticleCentered"> <input type="text" name="category" class="smallForm" value="<?php echo $data->category; ?>" required> </td>
                             </tr>
 
                             <tr>
@@ -69,14 +70,14 @@
                                     Quantity
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="number" name="quantity" class="smallForm" min="1" required> </td>
+                                <td class="verticleCentered"> <input type="number" name="quantity" class="smallForm" min="1" value="<?php echo $data->quantity; ?>" required> </td>
 
 
                                 <td class="verticleCentered">
                                     Unit Price
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="number" name="unitPrice" class="smallForm" min="1" required> </td>
+                                <td class="verticleCentered"> <input type="number" name="unitPrice" class="smallForm" min="1" value="<?php echo $data->unitPrice; ?>" required> </td>
                             </tr>
 
                             <tr>
@@ -84,61 +85,24 @@
                                     Manufacturer Date
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="Date" class="orderdetails" placeholder="YYYY-MM-DD" name="manu_date" required> </td>
+                                <td class="verticleCentered"> <input type="Date" class="orderdetails" placeholder="YYYY-MM-DD" name="manufacturedDate" value="<?php echo $data->manufacturedDate; ?>" required> </td>
 
                                 <td class="verticleCentered">
                                     Expire Date
                                 </td>
                                 <td> : </td>
-                                <td class="verticleCentered"> <input type="Date" class="orderdetails" placeholder="YYYY-MM-DD" name="expire_date" required> </td>
+                                <td class="verticleCentered"> <input type="Date" class="orderdetails" placeholder="YYYY-MM-DD" name="expireDate" value="<?php echo $data->expireDate; ?>" required> </td>
                             </tr>
 
                             <tr>
-                                <td class="verticleCentered"> <input type="submit" class="addBtn" value="Send"></td>
+                                <td class="verticleCentered"> <input type="submit" class="addBtn" value="Add" name="add"></td>
                                 <td><a href="<?php echo URLROOT ?>/pharmacies/inventory" class="link">
-                                    <div class="publicbtn"> Go Back </div>
-                                    </td>
+                                        <div class="publicbtn"> Go Back </div>
+                                </td>
                                 </a>
                             </tr>
                     </form>
-                    <!-- <form action="<?php echo URLROOT; ?>/pharmacies/addInventory" method="post" class="orderform">
-                        <div>
-                            <label for="medicineId">Medicine ID:</label>
-                            <input type="text" id="medicineId" name="medicineId" required>
-                        </div>
-                        <div>
-                            <label for="medicineName">Medicine Name:</label>
-                            <input type="text" id="medicineName" name="medicineName" required>
-                        </div>
-                        <div>
-                            <label for="batchNo">Batch Number:</label>
-                            <input type="text" id="batchNo" name="batchNo" required>
-                        </div>
-                        <div>
-                            <label for="category">Category:</label>
-                            <input type="text" id="category" name="category" required>
-                        </div>
-                        <div>
-                            <label for="quantity">Quantity:</label>
-                            <input type="text" id="quantity" name="quantity" required>
-                        </div>
-                        <div>
-                            <label for="manufacturedDate">Manufactured Date:</label>
-                            <input type="date" id="manufacturedDate" name="manufacturedDate" placeholder="YYYY-MM-DD" required>
-                        </div>
-                        <div>
-                            <label for="expireDate">Expired Date:</label>
-                            <input type="date" id="expireDate" name="expireDate" placeholder="YYYY-MM-DD" required>
-                        </div>
-                        <div>
-                            <label for="unitPrice">Unit Price:</label>
-                            <input type="text" id="unitPrice" name="unitPrice" required>
-                        </div>
-                        <div>
-                        <button type="submit">Add Inventory</button>
-                        <button type="submit"><a href="<?php echo URLROOT ?>/pharmacies/inventory" class="link">Back</a></button>
-                        </div>
-                    </form> -->
+
 
 
                     </table>
